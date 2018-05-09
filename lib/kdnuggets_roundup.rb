229ea750ddedccc133c7490cf96c5974be551139
Jirles <emily.jirles@gmail.com>
2018-05-08 21:15:49 -0400

    Setting up CLI class includign #call, #menu

--- lib/kdnuggets_roundup.rb
+++ lib/kdnuggets_roundup.rb
@@ -1,5 +1,21 @@
 require "kdnuggets_roundup/version"
 
 module KdnuggetsRoundup
-  # Your code goes here...
+
+  class RoundupCLI
+    def call
+      puts "Howdy, partner!"
+      puts "The Kdnuggets Roundup is your source for the top articles in data science."
+      puts "Let's see what's hot this week!"
+      menu
+    end
+
+    def menu
+      puts "this will be the menu"
+    end
+
+  end
+
+
+
 end
